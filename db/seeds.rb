b03177793e44096297067e4a94de0ca04b9d62f0
jadeson komolvipat <jadesonk@gmail.com>
2020-09-09 13:50:48 +0700

    seed for fake restaurant data using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  restaurant = {
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  }
+  new_restaurant = Restaurant.create!(restaurant)
+  puts "Created #{new_restaurant.name}"
+end
+puts 'Finished!'
