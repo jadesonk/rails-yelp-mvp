4e3753b65985bad23aa60823d099afbc201156fd
jadeson komolvipat <jadesonk@gmail.com>
2020-09-09 14:07:39 +0700

    setup initial routes, generate controllers, and create index view for restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'restaurants#index'
+  resources :restaurants, except: %i[destroy edit update] do
+    resources :reviews, only: %i[new create]
+  end
+
+  resources :reviews, only: [:index]
 end
