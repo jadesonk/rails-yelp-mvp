b5e723220d3e41e2c5a6e97a4706ea37ead8318a
jadeson komolvipat <jadesonk@gmail.com>
2020-09-09 14:42:41 +0700

    complete add add a review flow

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant, only: %i[new create]
+
+  def new
+    @review = Review.new
+    @review.restaurant = @restaurant
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to @restaurant
+    else
+      render :new # need to find syntax for how to redirect to show page
+    end
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
