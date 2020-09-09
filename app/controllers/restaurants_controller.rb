5f804d9e45ffb510fafaa0489486458363cdd1ce
jadeson komolvipat <jadesonk@gmail.com>
2020-09-09 14:29:50 +0700

    add create new restaurant flow

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,9 +8,27 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @categories = %w[chinese italian japanese french belgian]
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
 
   def find_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
